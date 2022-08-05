5942b850dcb1d2e4b9a6fc6a82ce776292071440
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2022-08-05 09:28:50 -0700

    Build initial tests

---
+++ cypress/e2e/RandomStats.cy.js
@@ -0,0 +1,39 @@
+describe('empty spec', () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://www.balldontlie.io/api/v1/stats?seasons[]=2020&player_ids[]=246&per_page=82", {
+      statusCode:201,
+      fixture:"Stats2020.json"
+    })
+    cy.intercept("GET", "https://www.balldontlie.io/api/v1/stats?seasons[]=2021&player_ids[]=246&per_page=82", {
+      statusCode:201,
+      fixture:"Stats2021.json"
+    })
+    cy.visit('http://localhost:3000/')
+  })
+  
+  it('should load landing page with correct header', () => {
+    cy.contains("sombor shuffle")
+    cy.contains("get a random statline from Nikola Jokic's MVP seasons")
+    cy.contains("points / rebounds / assists")
+  })
+
+  it('should show points, rebounds and assists', () => {
+    cy.contains("29")
+    cy.contains("15")
+    cy.contains("14")
+  })
+
+  it("should have button to get new random statline and a save button", () => {
+    cy.get("button").first()
+    cy.contains("get new random statline")
+    cy.get("button").last()
+    cy.contains("save this statline")
+  })
+
+  it("should show show saved button only when stat has been saved", () => {
+    cy.get(".show-saved-button").should("not.exist")
+    cy.get("button").last().click()
+    cy.get(".show-saved-button")
+    cy.contains("show saved")
+  })
+})

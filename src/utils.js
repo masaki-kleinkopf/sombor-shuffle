a020a8eaea5aff384c65e31121331906ccf1ece3
Masaki Kleinkopf <masaki.kleinkopf@gmail.com>
2023-02-14 11:02:34 -0800

    sort games by date

--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,37 @@
 import dayjs from "dayjs";
 
 const mapData = (seasonsData) => {
-  return seasonsData.map((data) => {
-    if (data.game.home_team_id === 8) {
-      return {
-        points: data.pts,
-        assists: data.ast,
-        rebounds: data.reb,
-        date: dayjs(data.game.date).format("MM/DD/YYYY"),
-        id: data.id,
-        opponent: data.game.visitor_team_id,
-        nuggets_score: data.game.home_team_score,
-        opponent_score: data.game.visitor_team_score,
-      };
-    } else {
-      return {
-        points: data.pts,
-        assists: data.ast,
-        rebounds: data.reb,
-        date: dayjs(data.game.date).format("MM/DD/YYYY"),
-        id: data.id,
-        opponent: data.game.home_team_id,
-        nuggets_score: data.game.visitor_team_score,
-        opponent_score: data.game.home_team_score,
-      };
-    }
-  });
+  return seasonsData
+    .map((data) => {
+      if (data.game.home_team_id === 8) {
+        return {
+          points: data.pts,
+          assists: data.ast,
+          rebounds: data.reb,
+          date: dayjs(data.game.date).format("MM/DD/YYYY"),
+          id: data.id,
+          opponent: data.game.visitor_team_id,
+          nuggets_score: data.game.home_team_score,
+          opponent_score: data.game.visitor_team_score,
+        };
+      } else {
+        return {
+          points: data.pts,
+          assists: data.ast,
+          rebounds: data.reb,
+          date: dayjs(data.game.date).format("MM/DD/YYYY"),
+          id: data.id,
+          opponent: data.game.home_team_id,
+          nuggets_score: data.game.visitor_team_score,
+          opponent_score: data.game.home_team_score,
+        };
+      }
+    })
+    .sort((a, b) => {
+      let dateA = new Date(a.date);
+      let dateB = new Date(b.date);
+      return dateA - dateB;
+    });
 };
 
 const mapTeams = (teamsData) => {
